6159e2f033440d4cd20e7f721f6f2e256a6bba65
user <user@DESKTOP-O8A11MC>
2026-08-24 09:54:52 +0900

    스케줄러 mapper null 방지 수정 - 김지연

--- syfc/src/main/java/com/syfc/listener/MatchSchedulerListener.java
+++ syfc/src/main/java/com/syfc/listener/MatchSchedulerListener.java
@@ -17,8 +17,7 @@ import com.syfc.service.MatchServiceImpl;
 @WebListener
 public class MatchSchedulerListener implements ServletContextListener {
 
-    private ScheduledExecutorService scheduler;
-    private MatchService service;
+	private ScheduledExecutorService scheduler;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
@@ -27,50 +26,76 @@ public class MatchSchedulerListener implements ServletContextListener {
         System.out.println("경기 자동취소 스케줄러 시작");
         System.out.println("====================================");
 
-        service = new MatchServiceImpl();
-
-        //  ① 서버 시작 즉시 한 번 실행
-        cancelExpiredMatches();
-
-        // ② 매일 자정 실행
+        // mapper null이 일어나서 서버시작하고 5초 후 실행하게 바꿈
         scheduler = Executors.newSingleThreadScheduledExecutor();
 
-        LocalDateTime now = LocalDateTime.now();
+        scheduler.schedule(
+            () -> cancelExpiredMatches(),
+            5,
+            TimeUnit.SECONDS
+        );
 
-        LocalDateTime nextMidnight =
-                LocalDateTime.of( now.toLocalDate().plusDays(1), LocalTime.MIDNIGHT);
-
-        long initialDelay = Duration.between(now, nextMidnight).toMillis();
-        long oneDay = TimeUnit.DAYS.toMillis(1);
-
-        scheduler.scheduleAtFixedRate(this::cancelExpiredMatches,
-                initialDelay,oneDay, TimeUnit.MILLISECONDS);
-
-        System.out.println("다음 자동취소 실행시간 : " + nextMidnight);
+        // 이후 매일 자정에 실행
+        scheduleNextMidnight();
     }
 
-    // 경기일이 지난 미매칭 경기 자동 취소
+    // 경기 자동 취소 실행
     private void cancelExpiredMatches() {
 
         try {
+            System.out.println();
+            System.out.println("====================================");
+            System.out.println("[경기 자동취소] 검사 시작");
+            System.out.println("실행시간 : " + LocalDateTime.now());
+            System.out.println("====================================");
+
+            //Listener 객체가 생성될 때 Service를 만들지 않는다.
+            // MapperContainer 초기화 이후에 생성하기 위해 여기서 service 만듬.
+            MatchService service = new MatchServiceImpl();
+
             int count = service.cancelExpiredWaitingMatches();
-            System.out.println( "[경기 자동취소] " + count + "건 처리 완료");
+
+            System.out.println("[경기 자동취소] 처리 완료 : " + count + "건");
 
         } catch (Exception e) {
-
-            System.err.println( "[경기 자동취소] 오류 발생");
-
+            System.out.println("[경기 자동취소] 오류 발생");
             e.printStackTrace();
         }
     }
 
+    // 다음 자정에 실행되도록 예약
+    private void scheduleNextMidnight() {
+
+        LocalDateTime now = LocalDateTime.now();
+
+        LocalDateTime nextMidnight = LocalDateTime.of(
+                    now.toLocalDate().plusDays(1), LocalTime.MIDNIGHT);
+
+        long delayMillis = Duration.between(now, nextMidnight).toMillis();
+
+        System.out.println("다음 자동취소 실행시간 : " + nextMidnight);
+
+        scheduler.schedule( () -> {
+                try {
+                    cancelExpiredMatches();
+                } finally {
+                    // 다시 다음날 자정 예약
+                    scheduleNextMidnight();
+                }
+            },
+            delayMillis,
+            TimeUnit.MILLISECONDS
+        );
+    }
+
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-
-        System.out.println( "경기 자동취소 스케줄러 종료");
+        System.out.println("====================================");
+        System.out.println("경기 자동취소 스케줄러 종료");
+        System.out.println("====================================");
 
         if (scheduler != null) {
-            scheduler.shutdown();
+            scheduler.shutdownNow();
         }
     }
 }
